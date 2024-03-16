3c0d4cca248e2be156d5021894838e1bb6fe9989
CainHu <cainhsui@gmail.com>
2024-03-16 14:14:16 +0800

    优化了预积分的计算以及修复了gyro和acc噪声互换了的问题

--- modules/vins/imu_integration.h
+++ modules/vins/imu_integration.h
@@ -15,50 +15,52 @@ namespace vins {
         EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 
         /**
-         * constructor, with initial bias a and bias g
-         * @param ba
-         * @param bg
-         */
-        explicit IMUIntegration(Vec3 ba, Vec3 bg);
+            * constructor, with initial bias a and bias g
+            * @param ba
+            * @param bg
+            */
+        explicit IMUIntegration(const Vec3 &acc_init, const Vec3 &gyro_init, const Vec3 &ba, const Vec3 &bg);
 
         ~IMUIntegration() = default;
 
+        void push_back(double dt, const Vec3 &acc, const Vec3 &gyro);
+
         /**
-         * propage pre-integrated measurements using raw IMU data
-         * @param dt
-         * @param acc
-         * @param gyr_1
-         */
+            * propage pre-integrated measurements using raw IMU data
+            * @param dt
+            * @param acc
+            * @param gyr_1
+            */
         void propagate(double dt, const Vec3 &acc, const Vec3 &gyro);
 
         /**
-         * according to pre-integration, when bias is updated, pre-integration should also be updated using
-         * first-order expansion of ba and bg
-         *
-         * @param delta_ba
-         * @param delta_bg
-         */
+            * according to pre-integration, when bias is updated, pre-integration should also be updated using
+            * first-order expansion of ba and bg
+            *
+            * @param delta_ba
+            * @param delta_bg
+            */
         void correct(const Vec3 &delta_ba, const Vec3 &delta_bg);
 
         void set_gyro_bias(const Vec3 &bg) { _bg = bg; }
-
         void set_acc_bias(const Vec3 &ba) { _ba = ba; }
+        static void set_gravity(const Vec3 &gravity) { _gravity = gravity; }
 
         /// if bias is update by a large value, redo the propagation
-        void repropagate();
+        void repropagate(const Eigen::Vector3d &ba, const Eigen::Vector3d &bg);
 
         /// reset measurements
         /// NOTE ba and bg will not be reset, only measurements and jacobians will be reset!
         void reset();
 
         /**
-         * get the jacobians from r,v,p w.r.t. biases
-         * @param _dr_dbg
-         * @param _dv_dbg
-         * @param _dv_dba
-         * @param _dp_dbg
-         * @param _dp_dba
-         */
+            * get the jacobians from r,v,p w.r.t. biases
+            * @param _dr_dbg
+            * @param _dv_dbg
+            * @param _dv_dba
+            * @param _dp_dbg
+            * @param _dp_dba
+            */
         void get_jacobians(Mat33 &dr_dbg, Mat33 &dv_dbg, Mat33 &dv_dba, Mat33 &dp_dbg, Mat33 &dp_dba) const {
             dr_dbg = _dr_dbg;
             dv_dbg = _dv_dbg;
@@ -68,52 +70,62 @@ namespace vins {
         }
 
         const Mat33 &get_dr_dbg() const { return _dr_dbg; }
+        const Mat33 &get_dp_dbg() const { return _dp_dbg; }
+        const Mat33 &get_dp_dba() const { return _dp_dba; }
+        const Mat33 &get_dv_dbg() const { return _dv_dbg; }
+        const Mat33 &get_dv_dba() const { return _dv_dba; }
 
-        /// get propagated noise covariance
-        const Mat99 &get_covariance_measurement() const { return _covariance_measurement; }
-
-        /// get random walk covariance
-        Mat66 get_covariance_randomWalk() const { return _noise_random_walk * _sum_dt; }
+        const Mat1515 &get_covariance() const { return _covariance; }
 
         /// get sum of time
         double get_sum_dt() const { return _sum_dt; }
 
         /**
-         * get the integrated measurements
-         * @param delta_r
-         * @param delta_v
-         * @param delta_p
-         */
+            * get the integrated measurements
+            * @param delta_r
+            * @param delta_v
+            * @param delta_p
+            */
         void get_delta_RVP(Sophus::SO3d &delta_r, Vec3 &delta_v, Vec3 &delta_p) const {
             delta_r = _delta_r;
             delta_v = _delta_v;
             delta_p = _delta_p;
         }
 
-        const Vec3 &get_dv() const { return _delta_v; }
+        const Vec3 &get_delta_v() const { return _delta_v; }
+        const Vec3 &get_delta_p() const { return _delta_p; }
+        const Sophus::SO3d &get_delta_r() const { return _delta_r; }
+        const Qd &get_delta_q() const { return _delta_r.unit_quaternion(); }
 
-        const Vec3 &get_dp() const { return _delta_p; }
+        const Vec3 &get_ba() const { return _ba; }
+        const Vec3 &get_bg() const { return _bg; }
+        static const Vec3 &get_gravity() { return _gravity; }
 
-        const Sophus::SO3d &get_dr() const { return _delta_r; }
+    protected:
+        void calculate_cov(const Mat33 &delta_r, const Mat33 &delta_r_last,
+                           const Mat33 &delta_r_a_hat, const Mat33 &delta_r_a_hat_last,
+                           const Mat33 &dr, const Mat33 &jr_dt);
 
     private:
         // raw data from IMU
         std::vector<double> _dt_buf;
         VecVec3 _acc_buf;
         VecVec3 _gyro_buf;
+        Vec3 _acc_init;
+        Vec3 _gyro_init;
         Vec3 _acc_last;
         Vec3 _gyro_last;
+
+        double _dt = 0.;
+        static Vec3 _gravity;
+        Vec3 _ba;
+        Vec3 _bg;
 
         // pre-integrated IMU measurements
         double _sum_dt = 0;
         Sophus::SO3d _delta_r;  // dR
         Vec3 _delta_v = Vec3::Zero();    // dv
         Vec3 _delta_p = Vec3::Zero();    // dp
-
-        // gravity, biases
-        static Vec3 _gravity;
-        Vec3 _bg = Vec3::Zero();    // initial bias of gyro
-        Vec3 _ba = Vec3::Zero();    // initial bias of accelerator
 
         // jacobian w.r.t bg and ba
         Mat33 _dr_dbg = Mat33::Zero();
@@ -123,14 +135,16 @@ namespace vins {
         Mat33 _dp_dba = Mat33::Zero();
 
         // noise propagation
-        Mat99 _covariance_measurement = Mat99::Zero();
-        Mat66 _covariance_random_walk = Mat66::Zero();
-        Mat99 _A = Mat99::Identity();
-        Mat96 _B = Mat96::Zero();
+        Mat1515 _covariance = Mat1515::Zero();
+
+        Eigen::Matrix<double, 9, 9> _A00 = Eigen::Matrix<double, 9, 9>::Identity();
+        Eigen::Matrix<double, 9, 6> _A01 = Eigen::Matrix<double, 9, 6>::Zero();
+        Eigen::Matrix<double, 9, 12> _B00 = Eigen::Matrix<double, 9, 12>::Zero();
+        Eigen::Matrix<double, 9, 6> _B01 = Eigen::Matrix<double, 9, 6>::Zero();
 
         // raw noise of imu measurement
-        Mat66 _noise_measurement = Mat66::Identity();
-        Mat66 _noise_random_walk = Mat66::Identity();
+        Eigen::Matrix<double, 12, 1> _noise_measurement = Eigen::Matrix<double, 12, 1>::Identity();
+        Vec6 _noise_random_walk = Vec6::Identity();
 
         /**@brief accelerometer measurement noise standard deviation*/
         constexpr static double _acc_noise = 0.2;
